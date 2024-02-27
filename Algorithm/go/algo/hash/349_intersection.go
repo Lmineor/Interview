d29c644ed5346714824aeafc53089271658f8958
lex <luohai2233@163.com>
2024-02-27 22:06:40 +0800

    vault backup: 2024-02-27 22:06:40

--- Algorithm/go/algo/hash/349_intersection.go
+++ Algorithm/go/algo/hash/349_intersection.go
@@ -6,8 +6,21 @@ package hash
 //
 //说明： 输出结果中的每个元素一定是唯一的。 我们可以不考虑输出结果的顺序。
 //
-//#
+//# 350.
+//给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+//
+//
+//
+//示例 1：
+//
+//输入：nums1 = [1,2,2,1], nums2 = [2,2]
+//输出：[2,2]
+//示例 2:
+//
+//输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出：[4,9]
 
+//349
 func intersection2(nums1 []int, nums2 []int) []int {
 	var result []int
 	resultMap := make(map[int]struct{}, 0)
@@ -26,6 +39,8 @@ func intersection2(nums1 []int, nums2 []int) []int {
 	return result
 }
 
+
+//350
 func intersection(nums1 []int, nums2 []int) []int {
 	var result []int
 	resultMap := make(map[int]int, 0)
