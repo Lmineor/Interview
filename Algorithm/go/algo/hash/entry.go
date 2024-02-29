d9ee2464be438f63b95166aafde0f03169b915b1
lex <luohai2233@163.com>
2024-02-29 22:55:17 +0800

    vault backup: 2024-02-29 22:55:16

--- Algorithm/go/algo/hash/entry.go
+++ Algorithm/go/algo/hash/entry.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func Entry(){
 	//fmt.Println(isAnagram("cat", "car"))
-	fmt.Println(intersection([]int{4,9,5}, []int{9,4,9,8,4}))
+	//fmt.Println(intersection([]int{4,9,5}, []int{9,4,9,8,4}))
+	fmt.Println(isHappy(19))
 }
 
