71971ec24cce9b926a2dafe4a6e7bbb92f306ef3
lex <luohai2233@163.com>
2024-03-04 21:41:42 +0800

    vault backup: 2024-03-04 21:41:42

--- Algorithm/go/algo/hash/entry.go
+++ Algorithm/go/algo/hash/entry.go
@@ -1,12 +1,12 @@
 package hash
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func Entry() {
 	//fmt.Println(isAnagram("cat", "car"))
 	//fmt.Println(intersection([]int{4,9,5}, []int{9,4,9,8,4}))
 	//fmt.Println(isHappy(19))
-	fmt.Println(canConstruct("a", "b"))
+	//fmt.Println(canConstruct("a", "b"))
+	nums := []int{-1,0,1,2,-1,-4}
+	fmt.Println(threeSum(nums))
 }
