a3bdf385be1d7ead141723c9f9af7af1651b31e7
lex <luohai2233@163.com>
2024-03-16 16:43:53 +0800

    vault backup: 2024-03-16 16:43:53

---
+++ Algorithm/go/algo/stack/20_isValid.go
@@ -0,0 +1,43 @@
+package stack
+
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+//有效字符串需满足：
+//
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//注意空字符串可被认为是有效字符串。
+
+func isValid(s string) bool {
+	var a = "("
+	var b = ")"
+	var c = "["
+	var d = "]"
+	var e = "{"
+	var f = "}"
+
+	stack := make([]string, 0)
+	for ss := range s{
+		switch string(ss) {
+		case a,c,e:
+			stack = append(stack, a)
+		case b:
+			if stack[len(stack)-1] != a{
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case d:
+			if stack[len(stack)-1] != c{
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case f:
+			if stack[len(stack)-1] != e{
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+
--- Algorithm/go/algo/stack/stack.go
+++ Algorithm/go/algo/stack/stack.go
@@ -4,20 +4,6 @@ import "fmt"
 
 func Entry(){
 	//fmt.Println(computeExpression("1-2*3/3"))
-	s := Constructor1()
-	//fmt.Println(s.Pop())
-	//fmt.Println(s.Peek())
-	s.Push(1)
-	s.Push(2)
-	//fmt.Println(s.Peek())
-	s.Push(3)
-	s.Push(4)
-	fmt.Println(s.Top())
-	fmt.Println(s.Pop())
-	s.Push(5)
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
+	fmt.Println(isValid("(]"))
 
 }
