cf0f318d214178c4205da3f6996ce1166c755fcf
lex <luohai2233@163.com>
2024-03-19 23:32:52 +0800

    vault backup: 2024-03-19 23:32:52

--- Algorithm/go/algo/stack/1047_removeDuplicates.go
+++ Algorithm/go/algo/stack/1047_removeDuplicates.go
@@ -1,4 +1,5 @@
 package stack
+
 //给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
 //
 //在 S 上反复执行重复项删除操作，直到无法继续删除。
@@ -11,9 +12,20 @@ package stack
 //输出："ca"
 //解释：例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
 
-
-
-
 func removeDuplicates(s string) string {
+	byteS := []byte(s)
+	if hasSame(byteS){
+		removeDuplicates(string(byteS))
+	}else{
+		return string(s)
+	}
+}
 
+func hasSame(bs []byte) bool {
+	for i := 1; i < len(bs); i++ {
+		if bs[i-1] == bs[i]{
+			return true
+		}
+	}
+	return false
 }
