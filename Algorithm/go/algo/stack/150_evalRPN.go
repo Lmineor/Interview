f4c7742c3308334511b5a245774a9a9c1a2a221d
lex <luohai2233@163.com>
2024-03-20 22:14:16 +0800

    vault backup: 2024-03-20 22:14:16

--- Algorithm/go/algo/stack/150_evalRPN.go
+++ Algorithm/go/algo/stack/150_evalRPN.go
@@ -1,8 +1,33 @@
 package stack
 
+const (
+	plus  = "+"
+	minus = "-"
+	cheng = "*"
+	chu   = "/"
+)
 
 func evalRPN(tokens []string) int {
-	for len(tokens)>1{
-
+	i := 0
+	for len(tokens) > 1 {
+		if !isNum(tokens[i]){
+			leftNum := tokens[i-2]
+			rightNum := tokens[i-1]
+			op = tokens[i]
+		}
 	}
+}
+
+func cal(leftNum, rightNum, op string)int{
+	
+}
+
+func isNum(c string) bool {
+	nums := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+	for _, num := range nums {
+		if c == num {
+			return true
+		}
+	}
+	return false
 }
