1ceaace3a5563a0f4db5b5b5dc0d0c8696f454a1
lex <luohai2233@163.com>
2024-03-21 21:24:19 +0800

    vault backup: 2024-03-21 21:24:19

--- Algorithm/go/algo/stack/239_maxSlidingWindow.go
+++ Algorithm/go/algo/stack/239_maxSlidingWindow.go
@@ -2,5 +2,25 @@ package stack
 
 
 func maxSlidingWindow(nums []int, k int) []int {
+	left:=0
+	right := k
+	result := make([]int, 0)
+	currentMax := max(nums[0:k])
+	result = append(result, currentMax)
+	for right < len(nums){
+		right++
+		if nums[right]>currentMax{
+			result = append(result, nums[right])
+		}
+	}
+}
 
+func max(nums []int)int{
+	result := nums[0]
+	for _,v := range nums{
+		if v > result{
+			result = v
+		}
+	}
+	return result
 }
