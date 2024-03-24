d5ef232ff925905fc64d3b5c587ef443ecf54f4e
lex <luohai2233@163.com>
2024-03-24 14:04:33 +0800

    vault backup: 2024-03-24 14:04:32

--- Algorithm/go/algo/stack/239_maxSlidingWindow.go
+++ Algorithm/go/algo/stack/239_maxSlidingWindow.go
@@ -1,25 +1,22 @@
 package stack
 
 func maxSlidingWindow(nums []int, k int) []int {
-	left := 0
-	right := k
 	result := make([]int, 0)
-	currentMax, index := max(nums[0:k])
-	result = append(result, currentMax)
-	for right < len(nums)-1 {
-		right++
-		left++
-		if nums[right] >= currentMax {
-			index = right
-		}
-		if index >= left {
-			result = append(result, nums[index])
-		} else {
-			currentMax, index = max(nums[left:right])
-			result = append(result, currentMax)
-		}
+	qq := ConQ()
+	for i := 0; i < k; i++ {
+		qq.Push(nums[i])
+	}
+	result = append(result, qq.Front())
+	for i:= k; i<len(nums); i++{
+		qq.Push(nums[i])
+		qq.Pop(nums[i-k])
+		result = append(result, qq.Front())
 	}
 	return result
+}
+
+func ConQ() *MyQ {
+	return &MyQ{ele: make([]int, 0)}
 }
 
 type MyQ struct {
@@ -40,7 +37,7 @@ func (q *MyQ) Back() int {
 func (q *MyQ) Front() int {
 	return q.ele[0]
 }
-func (q *MyQ) Pop(value int) int {
+func (q *MyQ) Pop(value int) {
 	//pop(value)：如果窗口移除的元素value等于单调队列的出口元素，那么队列弹出元素，否则不用任何操作
 	if !q.Empty() && q.Front() == value {
 		q.ele = q.ele[1:]
