a3d1e6a6d0744c65ef0926879bcb13cd64720c7e
lex <luohai2233@163.com>
2024-03-24 13:59:57 +0800

    vault backup: 2024-03-24 13:59:57

--- Algorithm/go/algo/stack/239_maxSlidingWindow.go
+++ Algorithm/go/algo/stack/239_maxSlidingWindow.go
@@ -26,18 +26,24 @@ type MyQ struct {
 	ele []int
 }
 
-func (q *MyQ)Push(value int){
-
+func (q *MyQ) Push(value int) {
+	//push(value)：如果push的元素value大于入口元素的数值，那么就将队列入口的元素弹出，直到push元素的数值小于等于队列入口元素的数值为止
+	for !q.Empty() && q.Back() < value {
+		q.ele = q.ele[:len(q.ele)-1]
+	}
+	q.ele = append(q.ele, value)
 }
 
+func (q *MyQ) Back() int {
+	return q.ele[len(q.ele)-1]
+}
 func (q *MyQ) Front() int {
 	return q.ele[0]
 }
 func (q *MyQ) Pop(value int) int {
 	//pop(value)：如果窗口移除的元素value等于单调队列的出口元素，那么队列弹出元素，否则不用任何操作
-	//push(value)：如果push的元素value大于入口元素的数值，那么就将队列入口的元素弹出，直到push元素的数值小于等于队列入口元素的数值为止
 	if !q.Empty() && q.Front() == value {
-		q.ele = q.ele[:len(q.ele)-1]
+		q.ele = q.ele[1:]
 	}
 }
 
