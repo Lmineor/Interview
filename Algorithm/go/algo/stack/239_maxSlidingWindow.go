51d9e4fb7fb4c2b93aacb4879e39fc5b8a5a4f10
lex <luohai2233@163.com>
2024-03-24 13:52:59 +0800

    vault backup: 2024-03-24 13:52:58

--- Algorithm/go/algo/stack/239_maxSlidingWindow.go
+++ Algorithm/go/algo/stack/239_maxSlidingWindow.go
@@ -1,21 +1,20 @@
 package stack
 
-
 func maxSlidingWindow(nums []int, k int) []int {
-	left:=0
+	left := 0
 	right := k
 	result := make([]int, 0)
 	currentMax, index := max(nums[0:k])
 	result = append(result, currentMax)
-	for right < len(nums)-1{
+	for right < len(nums)-1 {
 		right++
 		left++
-		if nums[right]>=currentMax{
+		if nums[right] >= currentMax {
 			index = right
 		}
-		if index >=left{
+		if index >= left {
 			result = append(result, nums[index])
-		}else{
+		} else {
 			currentMax, index = max(nums[left:right])
 			result = append(result, currentMax)
 		}
@@ -23,12 +22,25 @@ func maxSlidingWindow(nums []int, k int) []int {
 	return result
 }
 
-type MyQueue struct {
+type MyQ struct {
 	ele []int
 }
 
-func (q *MyQueue)Pop()int{
+func (q *MyQ)Push(value int){
+
+}
+
+func (q *MyQ) Front() int {
+	return q.ele[0]
+}
+func (q *MyQ) Pop(value int) int {
 	//pop(value)：如果窗口移除的元素value等于单调队列的出口元素，那么队列弹出元素，否则不用任何操作
 	//push(value)：如果push的元素value大于入口元素的数值，那么就将队列入口的元素弹出，直到push元素的数值小于等于队列入口元素的数值为止
-	
+	if !q.Empty() && q.Front() == value {
+		q.ele = q.ele[:len(q.ele)-1]
+	}
+}
+
+func (q *MyQ) Empty() bool {
+	return len(q.ele) == 0
 }
